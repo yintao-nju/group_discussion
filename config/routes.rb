03eddde646d58b4f51154e5204f8d0664c83c349
yuanyinchun <yuanyinchun3@sina.com>
2012-12-21 12:06:41 +0800

    add controller welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 GroupDiscussion::Application.routes.draw do
+  get "welcome/index"
+
+  root :to => 'welcome#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

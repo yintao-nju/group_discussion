18600bd312cc7f9c32dd1f1247aea32c9d261a9c
yuanyinchun <yuanyinchun3@sina.com>
2012-12-21 10:56:49 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupDiscussion::Application.config.secret_token = '7397a03373c559b6db5335fb269ed95440be4901ee9b258697dfd8bfb15b8497ffe0ded0027e376b120c4dd791f07b47c7b3c926b9d658bc45c86ed4b4e5d5d2'
